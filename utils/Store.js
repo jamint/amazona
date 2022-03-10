b16590292696e207df592dbccd3725291bfc24af
Jad Mintun <jadmintun@gmail.com>
2022-03-10 11:33:26 -0500

    add shipping address

--- utils/Store.js
+++ utils/Store.js
@@ -7,8 +7,9 @@ const initialState = {
   darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
   cart: {
     cartItems: Cookies.get('cartItems') ? JSON.parse(Cookies.get('cartItems')) : [],
-    userInfo: Cookies.get('userInfo') ? JSON.parse(Cookies.get('userInfo')) : null,
+    shippingAddress: Cookies.get('shippingAddress') ? JSON.parse(Cookies.get('shippingAddress')) : {},
   },
+  userInfo: Cookies.get('userInfo') ? JSON.parse(Cookies.get('userInfo')) : null,
 }
 
 function reducer(state, action) {
@@ -18,7 +19,6 @@ function reducer(state, action) {
     case 'DARK_MODE_OFF':
       return { ...state, darkMode: false }
     case 'CART_ADD_ITEM': {
-      console.log(state)
       const newItem = action.payload
       const existItem = state.cart.cartItems.find((item) => item._id === newItem._id)
       const cartItems = existItem
@@ -32,10 +32,16 @@ function reducer(state, action) {
       Cookies.set('cartItems', JSON.stringify(cartItems))
       return { ...state, cart: { ...state.cart, cartItems } }
     }
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: { ...state.cart, shippingAddress: action.payload },
+      }
     case 'USER_LOGIN':
       return { ...state, userInfo: action.payload }
     case 'USER_LOGOUT':
       return { ...state, userInfo: null, cart: { cartItems: [] } }
+
     default:
       return state
   }
