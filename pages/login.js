ddbf3cc22840df6bae18038ae2e52b98542bb138
Jad Mintun <jadmintun@gmail.com>
2022-03-21 12:44:12 -0400

    updates

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ import { List, ListItem, Typography, TextField, Button, Link } from '@material-u
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import NextLink from 'next/link'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Layout from '../components/Layout'
 import { Store } from '../utils/Store'
 import useStyles from '../utils/styles'
@@ -107,7 +107,7 @@ export default function Login() {
             </Button>
           </ListItem>
           <ListItem>
-            Don't have an account? &nbsp;
+            Don&nbsp;t have an account? &nbsp;
             <NextLink href={`/register?redirect=${redirect || '/'}`} passHref>
               <Link>Register</Link>
             </NextLink>
--- utils/Store.js
+++ utils/Store.js
@@ -2,12 +2,12 @@ import Cookies from 'js-cookie'
 import { createContext, useReducer } from 'react'
 
 export const Store = createContext()
-
 const initialState = {
   darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
   cart: {
     cartItems: Cookies.get('cartItems') ? JSON.parse(Cookies.get('cartItems')) : [],
     shippingAddress: Cookies.get('shippingAddress') ? JSON.parse(Cookies.get('shippingAddress')) : {},
+    paymentMethod: Cookies.get('paymentMethod') ? Cookies.get('paymentMethod') : '',
   },
   userInfo: Cookies.get('userInfo') ? JSON.parse(Cookies.get('userInfo')) : null,
 }
